d493e4f110775a07e4487d880609b03efd69686b
hulunbao <hulunbao@gmail.com>
2019-01-21 19:14:48 +0800

    online_nosts

--- online_notes/urls.py
+++ online_notes/urls.py
@@ -6,4 +6,10 @@ from . import views
 urlpatterns = [
     # 主页
     url(r'^$',views.index,name='index'),
+
+    # 显示所有的主题
+    url(r'^topics/$',views.topics,name='topics'),
+
+    # 特定主题的详细页面
+    url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
 ]
--- online_notes/views.py
+++ online_notes/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """学习笔记主页"""
     return render(request,'online_notes/index.html')
+
+def topics(request):
+    """显示所有的主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request,'online_notes/topics.html',context)
+
+def topic(request,topic_id):
+    """显示主题对应的内容"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic,'entries': entries}
+    return render(request,'online_notes/topic.html',context)
