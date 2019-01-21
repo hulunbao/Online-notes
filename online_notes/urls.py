9f440197053e1e848259f77da483a3374937eae4
hulunbao <hulunbao@gmail.com>
2019-01-22 01:19:26 +0800

    online_notes

--- online_notes/urls.py
+++ online_notes/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
 
     # 特定主题的详细页面
     url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+
+    # 添加主题
+    url(r'^new_topic/$',views.new_topic,name='new_topic'),
+
+    # 用于添加新条目的界面
+    url(r'^new_entry/(?P<topic_id>\d+)/$',views.new_entry,name='new_entry'),
 ]
