5a2ac72df5f9424e98446cc2ecc256ff15831939
hulunbao <hulunbao@gmail.com>
2019-01-25 01:16:52 +0800

    users

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^login/$',login,{'template_name': 'users/login.html'},name='login'),
     # 注销
     url(r'^logout/$',views.logout_view,name='logout'),
+    # 注册页面
+    url(r'^register/$',views.register,name='register'),
 ]
